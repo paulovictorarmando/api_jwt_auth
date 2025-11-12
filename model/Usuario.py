69a9648f06a94ed3f9a4c9f94220c55d4c43c264
paulovictorarmando <paulovictorarmando@gmail.com>
2025-11-12 23:19:15 +0000

    create my user model

---
+++ model/Usuario.py
@@ -0,0 +1,8 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+
+class Usuario(SQLModel, table=True):
+	id: Optional[int] = Field(default=None, primary_key=True)
+	username: str = Field(max_lentgh=50, nullable=False)
+	email: str = Field(unique=True, nullable=False)
+	senha: str = Field(nullable=False)
