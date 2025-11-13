23cd70d7876d0697714170f81508cb58169ef552
paulovictorarmando <paulovictorarmando@gmail.com>
2025-11-13 06:31:44 +0000

    configuration for bd, connection and ENV, postgresql

---
+++ config/connection.py
@@ -0,0 +1,11 @@
+from sqlmodel import SQLModel, create_engine, Session
+from .settings import ENV
+
+engine = create_engine(ENV.DATABASE_URL, echo=True)
+
+def create_tables():
+	SQLModel.metadata.create_all(engine)
+
+def session():
+	with Session(engine) as s:
+		yield s
