7401f9cda7fa3d03e70f83fa43035f3693d2a7d6
paulovictorarmando <paulovictorarmando@gmail.com>
2025-11-13 06:18:27 +0000

    dto for model done

---
+++ dto/UsuarioDTO.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Annotated, Optional
+
+class UsuarioRead(BaseModel):
+	id: int
+	username: str
+	email: Optional[str] = None
+	model_config = {"from_attributes": True}
+
+class UsuarioCreate(BaseModel):
+	username: Annotated[str, Field(max_length=50)]
+	email: EmailStr
+	senha: Annotated[ str, Field(min_length=5)] = None
+
+class UsuarioLogin(BaseModel):
+	email: EmailStr
+	senha: str
+
+class UsuarioUpdate(BaseModel):
+	username: Optional[Annotated[Field(max_length=50)]] = None
+	email: Optional[EmailStr] = None
+	senha: Optional[Annotated[Field(min_length=5)]] = None
